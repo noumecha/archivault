d0ac87e0f85d3aa29ccdbf42bd1cba55dbc15bac
noumel <noumechaiva@gmail.com>
2026-05-02 11:52:41 +0100

    update configuration for medias in production

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,6 @@
 # config/urls.py
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.static import serve
