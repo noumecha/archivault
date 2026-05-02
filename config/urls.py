b2c0d5f2c6db82c9f3992aca076284f45bd1b43a
noumel <noumechaiva@gmail.com>
2026-05-02 11:40:22 +0100

    update configuration for medias in production

--- config/urls.py
+++ config/urls.py
@@ -3,6 +3,8 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
+import re
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -54,6 +56,11 @@ handler403 = "config.views.custom_permission_denied_view"
 handler404 = "config.views.custom_page_not_found_view"
 handler500 = "config.views.custom_server_error_view"
 
-# form files
+# for files in production and dev
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    # Option pour forcer le service des médias par Django en prod (via WhiteNoise)
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    ]
