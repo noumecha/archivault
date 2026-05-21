17d68a2d46922811250b8c96556169efb6b5e940
noumel <noumechaiva@gmail.com>
2026-05-21 12:42:46 +0100

    remove obligatory critera on theme

--- apps/documents/models.py
+++ apps/documents/models.py
@@ -105,7 +105,7 @@ class Document(models.Model):
     fichier = models.FileField(upload_to='documents/')
     type_document = models.ForeignKey(TypeDocument, on_delete=models.SET_NULL, null=True)
     sous_type = models.ForeignKey(SousTypeDocument, on_delete=models.SET_NULL, null=True, blank=True)
-    theme = models.ForeignKey(Theme, on_delete=models.SET_NULL, null=True)
+    theme = models.ForeignKey(Theme, on_delete=models.SET_NULL, null=True, blank=True)
     cellule = models.ForeignKey(Cellule, on_delete=models.SET_NULL, null=True, blank=True)
     etat = models.CharField(max_length=20, choices=EtatDocument.choices, default=EtatDocument.EN_ATTENTE)
     niveau_acces = models.CharField( max_length=20, choices=NiveauAcces.choices, default=NiveauAcces.INTERNE )
